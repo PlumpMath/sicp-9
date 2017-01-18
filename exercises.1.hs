0a5272feaf446af5c1795f615fb0c15f9d3c8ba3
Rafael Avila <rafa.avim@gmail.com>
2017-01-18 17:57:20 -0600

    refactor to use guards

--- exercises.1.hs
+++ exercises.1.hs
@@ -9,9 +9,7 @@ larger :: Ord t => t -> t -> t
 larger a b = if a > b then a else b
 
 sumSqr :: (Ord a, Num a) => a -> a -> a -> a
-sumSqr a b c =
-  if a > b && a > c then square a + square(larger b c)
-  else
-    if b > a && b > c then square b + square(larger a c)
-    else
-      square c + square(larger a b)
+sumSqr a b c
+  | a > b && a > c = square a + square(larger b c)
+  | b > a && b > c = square b + square(larger a c)
+  | otherwise = square c + square(larger a b)
