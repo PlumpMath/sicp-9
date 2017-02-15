8715328db6f372beb642593ce6e33a4b773864e9
Rafael Avila <rafa.avim@gmail.com>
2017-02-14 21:24:11 -0600

    add exerices 1.11

---
+++ exercise-1-11.hs
@@ -0,0 +1,17 @@
+module Exercises
+    (
+    ) where
+
+f :: Integral n => n -> n
+f n
+  | n < 3 = n
+  | otherwise = f(n -  1) + 2 * f(n - 2) + 3 * f(n - 3)
+
+helper :: Integral i => [i] -> [i]
+helper is
+  | null is = []
+  | otherwise = [ x + y | x <- is, y <- tail is]
+
+reduce :: [Int] -> [Int]
+reduce (x:rest@(y:z)) = (x + y) : (reduce rest)
+reduce _ = []
